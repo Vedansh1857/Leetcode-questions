baa5bb7af4d4f3bf6c4aa9ae8a796da3cc640d8a
Vedansh Gupta <vedanshgupta606@gmail.com>
2023-02-06 10:58:50 +0530

    Create Solution.cpp

---
+++ Medium/Wiggle subsequence/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        
+        if(nums.size()<2)  return 1;
+        int dec=1;
+        int inc=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]) dec = inc+1;
+            if(nums[i]<nums[i-1]) inc = dec+1;
+        }
+        int ans = max(dec,inc);
+        return ans;
+    }
+};
